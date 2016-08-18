b36e47f1e716d13986afe7d91b4de599902b0452
Daniel Weber <dw@thedanielweber.com>
2016-08-18 15:32:13 +0000

    Initialzed

--- app/app.component.ts
+++ app/app.component.ts
@@ -4,7 +4,10 @@ import { ROUTER_DIRECTIVES } from '@angular/router';
 @Component({
     directives: [ROUTER_DIRECTIVES],
     selector: 'my-app',
-    template: `<router-outlet></router-outlet>`
+    template: `
+<h1>My First {{name}} app</h1>
+<router-outlet></router-outlet>
+<a [routerLink]="['/']">Home</a> | <a [routerLink]="['/about']">About</a>`,
 })
 export class AppComponent {
     name: string = "Angular 2 on Express";
